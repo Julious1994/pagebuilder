e205315aea133a71644e54f0ee48af0e44fafd45
Amit Patel <mtpatel557@yahoo.com>
2019-06-26 07:54:23 +0530

    Create client side react project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Builder from './core/builder';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Builder />
+    </div>
+  );
+}
+
+export default App;
