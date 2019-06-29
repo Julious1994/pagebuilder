86844e6b39ec462c7b1069d9acd92e86a5d76f35
Amit Patel <mtpatel557@yahoo.com>
2019-06-29 17:23:00 +0530

    Create input box style for setting panel

---
+++ client/src/components/PropertyInput.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+	width: 99%;
+	background-color: #949CA9;
+	border: none;
+	height: 30px;
+	color: white;
+`;
+
+export default Input;
