7119c0c7a81b4c028bd2d4388f8a9c1a24c92c71
Amit Patel <mtpatel557@yahoo.com>
2019-07-07 12:31:10 +0530

    Create actions and action types for changing settings

--- client/src/store/actionType.js
+++ client/src/store/actionType.js
@@ -1,2 +1,8 @@
 export const HIDE_SETTING  = 'HIDE_SETTING';
 export const SHOW_SETTING  = 'SHOW_SETTING';
+export const CHANGE_TITLE = 'CHANGE_TITLE';
+export const CHANGE_LOGO = 'CHANGE_LOGO';
+export const SET_COMPONENT = 'SET_COMPONENT';
+export const SET_HEADER = 'SET_HEADER';
+export const CHANGE_PAGE_CONTENT = 'CHANGE_PAGE_CONTENT';
+export const CHANGE_SETTING = 'CHANGE_SETTING';
--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,12 +1,39 @@
-import { SHOW_SETTING, HIDE_SETTING } from './actionType';
+import {
+	SHOW_SETTING,
+	HIDE_SETTING,
+	CHANGE_TITLE,
+	CHANGE_LOGO,
+	SET_COMPONENT,
+	CHANGE_SETTING
+} from './actionType';
 
-export const openSetting = (level) => {
-	console.log(level);
+export const openSetting = (block, level, index, type) => {
+	console.log(index);
 	return {
 	type: SHOW_SETTING,
-	payload: { level },
+	payload: { block, level, index, type },
 }};
 
 export const hideSetting = () => ({
 	type: HIDE_SETTING,
+});
+
+export const changeTitle = (title) => ({
+	type: CHANGE_TITLE,
+	payload: { title }
+});
+
+export const changeLogo = (logo) => ({
+	type: CHANGE_LOGO,
+	payload: { logo },
+});
+
+export const setComponent = (component) => ({
+	type: SET_COMPONENT,
+	payload: { component },
+});
+
+export const onSettingChange = (block, level, index, type) => ({
+	type: CHANGE_SETTING,
+	payload: { block, level, index, type},
 });
