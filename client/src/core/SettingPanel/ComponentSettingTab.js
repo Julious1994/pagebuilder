5ca5c15b166686311567e0909009047a1a1fb783
Amit Patel <mtpatel557@yahoo.com>
2019-07-13 20:37:54 +0530

    Create abosulte setting tab for component

---
+++ client/src/core/SettingPanel/ComponentSettingTab.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle, faSortUp, faCog, faSortDown } from '@fortawesome/free-solid-svg-icons';
+
+import { closeComponentSetting } from './../../store/actions';
+
+const TabWrapper = styled.div`
+	position: absolute;
+	right: 10px;
+	border-radius: 40px;
+	top: 10px;
+	background-color: #1F1F1F;
+`;
+
+const MenuIcon = styled(FontAwesomeIcon)`
+	padding: 15px 25px;
+	width: 25px;
+	height: 25px;
+	cursor: pointer;
+	color: ${props => props.color || '#fff'};
+`;
+
+class ComponentSettingTab extends React.Component {
+	render() {
+		const { closeComponentSetting, openSetting } = this.props;
+		return(
+			<TabWrapper>
+				<MenuIcon
+					icon={faSortUp}
+					onClick={() => }
+				/>
+				<MenuIcon
+					icon={faSortDown}
+				/>
+				<MenuIcon
+					icon={faCog}
+					onClick={(e) => {
+						e.stopPropagation();
+						openSetting();
+					}}
+				/>
+				<MenuIcon
+					icon={faTimesCircle}
+					color="red"
+					onClick={(e) => {
+						e.stopPropagation();
+						closeComponentSetting();
+					}}
+				/>
+			</TabWrapper>
+		)
+	}
+}
+
+const mapStateToDispatch = dispatch => ({
+	closeComponentSetting: () => dispatch(closeComponentSetting()),
+});
+
+export default connect(null, mapStateToDispatch)(ComponentSettingTab);
