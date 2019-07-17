1c7f1a4e036830e9539d292efe6bead24c798920
Amit Patel <mtpatel557@yahoo.com>
2019-07-17 21:24:04 +0530

    Add pageSetting and globalSetting

---
+++ client/src/core/settingProperties.js
@@ -0,0 +1,11 @@
+export const pageSettings = {
+	backgroundImage: 'url',
+	backgroundColor: 'color',
+	path: 'string',
+}
+
+export const globalSettings = {
+	domain: 'string',
+	backgroundImage: 'url',
+	backgroundColor: 'color',
+}
