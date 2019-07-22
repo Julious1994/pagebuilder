fa50bf33d5e7edcd05578a19dc0ad9b4f91e9052
Amit Patel <mtpatel557@yahoo.com>
2019-07-22 21:37:45 +0530

    Improve Input component

--- client/src/components/PropertyInput.js
+++ client/src/components/PropertyInput.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import styled from 'styled-components';
 
-const Input = styled.input`
+const InputWrapper = styled.input`
 	width: 99%;
 	background-color: #949CA9;
 	border: none;
 	height: 30px;
 	color: white;
 `;
+
+function Input({ value, onChange, name }) {
+	return (
+		<InputWrapper
+			value={value}
+			name={name}
+			onChange={(e) => onChange && onChange(e.target.value)}
+		/>
+	)
+}
 
 export default Input;
