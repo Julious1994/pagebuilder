8ab53074cfc046b65749ce6634825546acbb4de1
Amit Patel <mtpatel557@yahoo.com>
2019-07-22 21:38:30 +0530

    Update template mappings

--- client/src/core/templateMapper.js
+++ client/src/core/templateMapper.js
@@ -1,16 +1,28 @@
 import Header from './../templates/header/Header';
 import Team from './../templates/content/Team';
+import Subscribe from './../templates/content/forms/Subscribe';
+import Pricing1 from './../templates/content/pricing/Pricing1';
+import ContactForm from './../templates/content/forms/Contact';
+
+import Footer1 from './../templates/footers/Footer';
+import Footer2 from './../templates/footers/Footer2';
+import Footer3 from './../templates/footers/Footer3';
 
 export const headers = {
 	1: { component: Header, thumbnail: '', title: 'Header 1', type: 'header'},
 }
 
 export const footers = {
-	1: { component: Header, thumbnail: '', title: 'Footer 1', type: 'footer'},
+	1: { component: Footer1, thumbnail: 'footer-center.png', title: 'Footer 1', type: 'footer'},
+	2: { component: Footer2, thumbnail: 'footer-black.png', title: 'Footer 2', type: 'footer'},
+	3: { component: Footer3, thumbnail: 'footer.jpg', title: 'Footer 3', type: 'footer'},
 }
 
 export const contents = {
-	1: { component: Team, thumbnail: '', title: 'Team 1', type: 'content'},
+	1: { component: Team, thumbnail: 'team.jpg', title: 'Team 1', type: 'content'},
+	2: { component: Subscribe, thumbnail: 'subscribe.jpg', title: 'Subscribe 1', type: 'content'},
+	3: { component: Pricing1, thumbnail: 'pricing.jpg', title: 'Pricing 1', type: 'content'},
+	4: { component: ContactForm, thumbnail: 'contact.jpg', title: 'Contact 1', type: 'content'},
 }
 
 export default {
