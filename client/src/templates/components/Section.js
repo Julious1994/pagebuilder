e79f99677a47f7200f33cc76dae23a8decffc80b
Amit Patel <mtpatel557@yahoo.com>
2019-07-24 22:23:50 +0530

    Improve section component

--- client/src/templates/components/Section.js
+++ client/src/templates/components/Section.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Section = styled.div`
-	width: ${props => props.width || '100%'};
-	background-color: ${props => props.backgroundColor || 'transparent'};
+const SectionWrapper = styled.div`
+	width: ${props => `${props.width}%` || '100%'};
 	${props => props.center ? 'margin: 0px auto;' : ''}
 `;
+
+const SectionOuter = styled.div`
+	background-color: ${props => props.backgroundColor || 'transparent'};
+`;
+
+function Section(props) {
+	return (
+		<SectionOuter
+			backgroundColor={props.backgroundColor}
+		>
+			<SectionWrapper
+				width={props.contentWidth}
+				center={props.center}
+			>
+				{props.children}
+			</SectionWrapper>
+		</SectionOuter>
+	)
+}
 
 export default Section;
