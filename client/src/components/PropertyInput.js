105f84ea835a5cec760e6a216b9c071c58822676
Amit Patel <mtpatel557@yahoo.com>
2019-07-28 23:10:59 +0530

    Add width property in input component

--- client/src/components/PropertyInput.js
+++ client/src/components/PropertyInput.js
@@ -2,20 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const InputWrapper = styled.input`
-	width: 99%;
+	width: ${props => props.width ? props.width : '99%'};
 	background-color: #949CA9;
 	border: none;
 	height: 30px;
 	color: white;
 `;
 
-function Input({ value, onChange, name, type }) {
+function Input({ value, onChange, name, type, width, ...props }) {
 	return (
 		<InputWrapper
+			width={width}
 			value={value}
 			type={type}
 			name={name}
 			onChange={(e) => onChange && onChange(e.target.value)}
+			{...props}
 		/>
 	)
 }
