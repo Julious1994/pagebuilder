d68210aca13629b4bdbcb9e234a50c2e52a0095b
Amit Patel <mtpatel557@yahoo.com>
2019-08-03 18:39:20 +0530

    Implement dialog to create new page/article

--- client/src/core/Toolbar/NewDialog/index.js
+++ client/src/core/Toolbar/NewDialog/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Modal from 'react-modal';
 import styled from 'styled-components';
 
+import Input from './../../../components/PropertyInput';
+
 Modal.defaultStyles.overlay.backgroundColor = 'rgba(0,0,0,0.7)';
 
 const customStyles = {
   content : {
     top                   : '20%',
-    left                  : '20%',
-    right                 : '20%',
+    left                  : '30%',
+    right                 : '30%',
     bottom                : 'auto',
   }
 };
@@ -42,9 +44,53 @@ const Title = styled.h2`
 	margin-top: 0px;
 `;
 
+const Field = styled.div`
+	display: flex;
+	padding: 5px 0px;
+	${props => props.justify ? `justify-content:${props.justify}` : ''}
+`;
+
+const FieldTitle = styled.div`
+	${props => props.lineHeight ? 'line-height: 2em' : ''}
+	width: ${props => props.width || '25%'};
+	font-weight: 500;
+`;
+
 class NewDialog extends React.Component {
+	state={
+		documentProps: {
+			isArticle: false,
+			// isPost: false,
+			slug: '/',
+			name: 'page',
+			draft: true,
+		}
+	}
+
+	componentDidUpdate() {
+		const { isArticle = false, isPost = false } = this.props;
+		const { documentProps } = this.state;
+		if(documentProps.isArticle !== isArticle) {
+			documentProps.isArticle = isArticle;
+			this.setState({ documentProps });
+		}
+	}
+
+	handleDocumentChange(field, value) {
+		const { documentProps } = this.state;
+		documentProps[field] = value;
+		this.setState({ documentProps });
+	}
+
+	handleCreate = () => {
+		const { documentProps } = this.state;
+		const { onCreate } = this.props;
+		onCreate && onCreate(documentProps);
+	}
+
 	render() {
 		const { isOpen, onClose } = this.props;
+		const { documentProps } = this.state;
 		return(
 			<Modal
 				isOpen={isOpen}
@@ -53,7 +99,40 @@ class NewDialog extends React.Component {
 				ariaHideApp={false}
 			>
 				<ContentWrapper>
-					<CancelButton onClick={onClose}>Close</CancelButton>
+					<Title> New {documentProps.isArticle ? 'Article' : 'Page'}</Title>
+					<Field>
+						<FieldTitle>
+							Name
+						</FieldTitle>
+						<Input
+							value={documentProps.name}
+							onChange={(value) => this.handleDocumentChange('name', value)}
+						/>
+					</Field>
+					<Field>
+						<FieldTitle>
+							Slug
+						</FieldTitle>
+						<Input
+							value={documentProps.slug}
+							onChange={(value) => this.handleDocumentChange('slug', value)}
+						/>
+					</Field>
+					<Field>
+						<FieldTitle width="11%" lineHeight="2em">
+							Draft
+						</FieldTitle>
+						<Input
+							width="25%"
+							type="checkbox"
+							onChange={() => this.handleDocumentChange('draft', !documentProps.draft)}
+							checked={documentProps.draft}
+						/>
+					</Field>
+					<Field justify="flex-end">
+						<CancelButton onClick={onClose}>Cancel</CancelButton>
+						<CancelButton onClick={this.handleCreate}>Create</CancelButton>
+					</Field>
 				</ContentWrapper>
 			</Modal>
 		)
