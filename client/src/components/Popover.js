779c8e543e22ee56cfecc93f6ad5f6c090cf4eb2
Amit Patel <mtpatel557@yahoo.com>
2019-08-03 10:56:28 +0530

    Improve popover wrapper

--- client/src/components/Popover.js
+++ client/src/components/Popover.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Popover from 'react-tiny-popover';
 
 
-class Popover extends React.Component {
+class PopoverWrapper extends React.Component {
 	render() {
 		const { isOpen, content } = this.props;
+		const dest = document.getElementById("editor-content");
 		return (
 			<Popover
 					isOpen={isOpen}
 					position={['top', 'bottom']} // preferred position
 					content={content}
+					contentDestination={dest}
 			>
 				{
 					this.props.children
@@ -19,4 +21,4 @@ class Popover extends React.Component {
 	}
 }
 
-export default Popover;
+export default PopoverWrapper;
