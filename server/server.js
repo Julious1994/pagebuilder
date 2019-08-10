c2b2becef2ddba6eef108e2c4656777ed89d49e4
Amit Patel <mtpatel557@yahoo.com>
2019-08-10 22:52:33 +0530

    Connect mongo db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
-var port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+var port = 3001;
+
+require('./rest.api')(app);
+
+mongoose.connect('mongodb://localhost/pagebuilder')
+.then(()=> {
+    console.log('Database connected');
+})
+.catch((error)=> {
+    console.log('Error connecting to database');
+});
  
 app.get("/", (req, res) => {
  res.send("Hello World");
