e610e162d2f2f87716b8eece0203a0df8297380a
Amit Patel <mtpatel557@yahoo.com>
2019-08-10 22:52:01 +0530

    Create service wrapper class

---
+++ client/src/services/http.js
@@ -0,0 +1,55 @@
+
+const joinURL = (baseURL, url) => {
+	return `${baseURL}/${url}`;
+}
+
+const Headers = {
+	"Accept": "application/json",
+	"Content-Type": "application/json",
+}
+
+class Service {
+	constructor() {
+			this.baseURL = "";
+	}
+
+	request(url, method = 'POST', data = null) {
+			url = joinURL(this.baseURL, url);
+			const options = {
+					headers: Headers,
+					method,
+			};
+			if(data) {
+					options.body = JSON.stringify({...data});
+			}
+			console.log('body', options);
+			return fetch(url, options);
+	}
+
+	getAll(url, data = {}) {
+			const method = "POST";
+			this.request(url, method, data);
+	}
+
+	get(url) {
+			const method = "GET";
+			return this.request(url, method).then(res => res.json()).catch(err => {
+					console.log('ERR:', err);
+					return { status: 0, error: "Network request failed"};
+			});
+	}
+
+	post(url, data) {
+		const method = 'POST';
+		return this.request(url, method, data).then(res => res.json()).catch(err => {
+			console.log('ERR:', err);
+			return { status: 0, error: "Network request failed"};
+		});
+	}
+
+	delete() {
+
+	}
+}
+
+export default Service;
