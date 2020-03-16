84c59149a181bc56b01be2fb4676343fd11d33d1
Amit Patel <amitpatel23894@gmail.com>
2020-03-16 21:19:37 +0530

    Add froala content block design

--- client/src/templates/content/contents/content2.js
+++ client/src/templates/content/contents/content2.js
@@ -17,44 +17,82 @@ const Description = styled.div`
 const Header = styled.h1`
 	text-align: center;
 `;
-const Title = styled(EditableDiv)`
+const Title = styled(EditableArea)`
 	outline: none;
 `;
 
 function Content2(props) {
 	const { settings, onChange, editable, onEdit } = props;
 	return(
-		<Section
-			center={settings.centerSection}
-			backgroundColor={settings.sectionBackground}
-		>
-			<ContentWrapper>
-					<Header>
-							<Title
-								html={settings.title}
-								disabled={!editable}
-								onChange={(e) => onChange('title', e.target.value)}
-							/>
-					</Header>
-					<Description>
-						<EditableArea
-							html={settings.description}
-							disabled={!editable}
-							toolbarStyle={{
-								right: 'unset',
-								width: '100%',
-								top: '-6em',
-							}}
-							onChange={(e) => onChange('description', e)}
-						/>
-					</Description>
-			</ContentWrapper>
-		</Section>
+		<section class="fdb-block">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-8 col-lg-7 col-xl-5 text-center">
+        <div class="fdb-box">
+          <div class="row">
+            <div class="col">
+              <h1>Log In</h1>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Email" />
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col">
+              <input type="password" class="form-control mb-1" placeholder="Password" />
+              <p class="text-right"><a href="https://www.froala.com">Recover Password</a></p>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col">
+              <button class="btn btn-outline-secondary" type="button">Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+		// <Section
+		// 	center={settings.centerSection}
+		// 	backgroundColor={settings.sectionBackground}
+		// >
+		// 	<ContentWrapper>
+		// 			<Header>
+		// 					<Title
+		// 						toolbarStyle={{
+		// 							right: 'unset',
+		// 							width: '100%',
+		// 							top: '-6em',
+		// 						}}
+		// 						html={settings.title}
+		// 						disabled={!editable}
+		// 						onChange={(e) => {
+		// 							console.log('e', e);
+		// 							onChange('title', e)}}
+		// 					/>
+		// 			</Header>
+		// 			<Description>
+		// 				<EditableArea
+		// 					html={settings.description}
+		// 					disabled={!editable}
+		// 					toolbarStyle={{
+		// 						right: 'unset',
+		// 						width: '100%',
+		// 						top: '-6em',
+		// 					}}
+		// 					onChange={(e) => onChange('description', e)}
+		// 				/>
+		// 			</Description>
+		// 	</ContentWrapper>
+		// </Section>
 	);
 }
 
 Content2.defaultSettings = {
-	title: 'Contacts with Form',
+	title: '<p style="text-align:center;">Contacts with Form</p>',
 	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum The first line of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum The first line of Lorem Ipsum.',
 	color: '',
 	backgroundColor: '',
