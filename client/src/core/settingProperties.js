ddc9ac416741aeaa4a644f534061cff94847e369
Amit Patel <amitpatel23894@gmail.com>
2020-03-16 22:48:01 +0530

    Add page level attributes

--- client/src/core/settingProperties.js
+++ client/src/core/settingProperties.js
@@ -2,11 +2,12 @@ export const pageSettings = {
 	backgroundImage: 'url',
 	backgroundColor: 'color',
 	path: 'string',
+	metaKeyword: 'text',
+	metaDescription: 'text',
 }
 
 export const globalSettings = {
 	backgroundColor: 'color',
 	domain: 'string',
 	backgroundImage: 'url',
-	isArticle: 'boolean',
 }
